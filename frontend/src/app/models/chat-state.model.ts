4fdadf27f923c89263400c39a99f57c5608ab12e
Lara Kanevsky <larakanevsky@gmail.com>
2025-03-07 07:46:29 -0300

    Retry

--- frontend/src/app/models/chat-state.model.ts
+++
@@ -1,8 +0,0 @@
-import { ShowChatMessage } from "./show-chat-message.model";
-
-export interface ChatState {
-    messages: ShowChatMessage[];
-    selectedMessage: ShowChatMessage | null;
-    loading: boolean;
-    error: string | null;
-  }
